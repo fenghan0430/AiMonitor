974a170f346d4a2a03a225ed3478a2a624d409de
fenghan0430 <fenghan0430@outlook.com>
2024-12-29 23:13:02 +0800

    update

---
+++ src/main/java/com/example/aimonitor/AiMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.aimonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aimonitor/AiMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aimonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
